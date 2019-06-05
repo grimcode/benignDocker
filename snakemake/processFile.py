73007399df2cbf3d4b70b5039d85d88129efcc12
Alex Janse <Alex.Janse@radboudumc.nl>
2019-06-05 14:11:51 +0200

    snakemake

--- snakemake/processFile.py
+++ snakemake/processFile.py
@@ -1,9 +1,16 @@
 import json
+import requests as req
 
 def processFile(input):
     file = open(input,"r")
     jsonFile = json.load(file)
-    
-
+    for mutations in jsonFile:
+        url = 'http://192.168.1.101/value=1'
+        resp = req.get(url)
+        results = resp.text
+        for repl in ["'","[","]","[","]"]:
+            results = results.replace(repl,"")
+        results = results.split(",")
+        print(results)
 
 processFile("example.json")
